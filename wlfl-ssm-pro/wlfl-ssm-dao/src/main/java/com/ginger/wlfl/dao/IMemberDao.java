328385927289823262da10741b7c602a91be92e8
ginger <1290089493@qq.com>
2020-08-07 18:22:39 +0800

    查询订单详情功能

---
+++ wlfl-ssm-pro/wlfl-ssm-dao/src/main/java/com/ginger/wlfl/dao/IMemberDao.java
@@ -0,0 +1,15 @@
+package com.ginger.wlfl.dao;
+
+import com.ginger.wlfl.pojo.Member;
+import org.apache.ibatis.annotations.Select;
+
+public interface IMemberDao {
+
+    /**
+     * 根据id查询会员
+     * @param memberId
+     * @return
+     */
+    @Select("select * from member where id = #{memberId}")
+    public Member findById(String memberId);
+}
---
+++ wlfl-ssm-pro/wlfl-ssm-dao/src/main/java/com/ginger/wlfl/dao/ITravellerDao.java
@@ -0,0 +1,22 @@
+package com.ginger.wlfl.dao;
+
+import com.ginger.wlfl.pojo.Traveller;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+public interface ITravellerDao {
+    /**
+     * 根据订单id查询查询旅客
+     * @param id
+     * @return
+     */
+    @Select("SELECT *" +
+            " FROM traveller " +
+            " WHERE id = ( " +
+                " SELECT travellerid " +
+                " FROM order_traveller " +
+                " WHERE orderid = #{ordersId} " +
+            ")")
+    public List<Traveller> findTravellerOrdersById(String id);
+}
